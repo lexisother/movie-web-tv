2cf8ee6ccd06bb78575cfd5ce4d83644cf41544a
Alyxia Sother <alyxia@riseup.net>
2023-11-12 17:41:55 +0100

    [App] Add a small padding util

--- app/src/main/java/dev/alyxia/movietv/App.kt
+++ app/src/main/java/dev/alyxia/movietv/App.kt
@@ -1,10 +1,32 @@
 package dev.alyxia.movietv
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.alyxia.movietv.ui.screens.Home
+import dev.alyxia.movietv.ui.utils.Padding
+
+val ParentPadding = PaddingValues(vertical = 16.dp, horizontal = 58.dp)
+
+@Composable
+fun rememberChildPadding(direction: LayoutDirection = LocalLayoutDirection.current): Padding {
+    return remember {
+        Padding(
+            start = ParentPadding.calculateStartPadding(direction) + 8.dp,
+            top = ParentPadding.calculateTopPadding(),
+            end = ParentPadding.calculateEndPadding(direction) + 8.dp,
+            bottom = ParentPadding.calculateBottomPadding()
+        )
+    }
+}
 
 @Composable
 fun App() {
